003d1901c475e6ad54009675fb53f63f581cf137
eunsook <irene@utransfer.com>
2022-03-21 15:36:42 +0900

    download 추가

--- app/src/main/java/kr/dada/torigin/MainActivity.kt
+++ app/src/main/java/kr/dada/torigin/MainActivity.kt
@@ -1,13 +1,15 @@
 package kr.dada.torigin
 
-import android.content.Intent
-import android.content.Intent.ACTION_VIEW
+import android.app.DownloadManager
+import android.content.Context
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import kr.dada.torigin.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        if(binding != null) {
+        if (binding != null) {
             binding.activityMainEtLink.text = null
         }
     }
@@ -34,24 +36,47 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             activityMainTvGo.setOnClickListener {
                 val link = activityMainEtLink.text.toString()
-                if(link.isNotBlank()) {
+                if (link.isNotBlank()) {
                     val origin = link.split("&fname=")[1]
 
-                    if(origin.isNotBlank()) {
+                    if (origin.isNotBlank()) {
                         val url = origin.replace("%3A", ":")
                             .replace("%2F", "/")
 
-//                        Toast.makeText(this@MainActivity, "url : $url", Toast.LENGTH_LONG).show()
-
-                        val intent = Intent(ACTION_VIEW, Uri.parse(url))
-                        startActivity(intent)
+//                        val intent = Intent(ACTION_VIEW, Uri.parse(url))
+//                        startActivity(intent)
+                        downloadImg(url)
                     } else {
-                        Toast.makeText(this@MainActivity, "올바른 링크를 입력해주세요!", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@MainActivity, "올바른 링크를 입력해주세요!", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 } else {
                     Toast.makeText(this@MainActivity, "링크를 입력해주세요!", Toast.LENGTH_SHORT).show()
                 }
             }
         }
+    }
+
+    private fun downloadImg(url: String) {
+        val fileName = System.currentTimeMillis().toString()
+        val outputFilePath: String =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath + "/$fileName.png"
+
+        val file = File(outputFilePath)
+        if (!file.parentFile.exists()) {
+            file.parentFile.mkdirs()
+        }
+//        val file = File(getExternalFilesDir(null), "$fileName.png")
+
+        val downloadmanager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager?
+        val request = DownloadManager.Request(Uri.parse(url)).apply {
+            setTitle("$fileName.png")
+            setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION)
+            setVisibleInDownloadsUi(false)
+            setAllowedOverMetered(true)
+            setAllowedOverRoaming(true)
+            setDestinationUri(Uri.fromFile(file))
+        }
+        downloadmanager!!.enqueue(request)
     }
 }
